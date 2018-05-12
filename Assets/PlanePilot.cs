93b9f4c8a81b3e7f8044d87aa58b3cff539c78fe
Mayur Bhakta <32318210+mBhakta95@users.noreply.github.com>
2018-05-12 11:04:28 -0500

    Update PlanePilot.cs

--- Assets/PlanePilot.cs
+++ Assets/PlanePilot.cs
@@ -15,17 +15,8 @@ public class PlanePilot : MonoBehaviour {
         float bias = 0.96f;
         Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1.0f - bias);
         Camera.main.transform.LookAt(transform.position + transform.forward * 30.0f);
-
-
         transform.position += transform.forward * Time.deltaTime * 40.0f;
-
-       // speed += transform.forward.y *  Time.deltaTime * 2.0f;
-
         transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal") );
-        float terrainHeight = Terrain.activeTerrain.SampleHeight(transform.position);
-        if(terrainHeight > transform.position.y)
-        {
-            transform.position = new Vector3(transform.position.x, terrainHeight, transform.position.z);
-        }
+        
     }
 }
